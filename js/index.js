43184405be608247089ad86761ffd30b984731ba
Maria Campo <mariacampo@wifi-131-179-49-39.host.ucla.edu>
2025-04-15 12:19:36 -0700

    Adding webpage

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#name",{
+    strings: ['Maria Campo Martins'],
+    typeSpeed: 80
+})
+
+document.getElementById('github-icon').addEventListener('click', function () {
+    window.location.href = 'https://github.com/maria-campo-martins';
+});
+
+document.getElementById('linkedin-icon').addEventListener('click', function () {
+    window.location.href = 'https://www.linkedin.com/in/maria-campo-martins-78b302251';
+});
