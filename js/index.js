88c7caffac09aaf7981f593fdffadab9dde2de52
Maria Campo <mariacampo@wifi-131-179-10-136.host.ucla.edu>
2025-04-18 12:32:58 -0700

    Final changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-var typed = new Typed("#name",{
+var typed = new Typed("#my-name",{
     strings: ['Maria Campo Martins'],
     typeSpeed: 80
 })
+
+document.getElementById('theme-toggle').checked = false;
+document.documentElement.setAttribute('data-theme', 'light');
 
 document.getElementById('github-icon').addEventListener('click', function () {
     window.location.href = 'https://github.com/maria-campo-martins';
@@ -9,4 +12,22 @@ document.getElementById('github-icon').addEventListener('click', function () {
 
 document.getElementById('linkedin-icon').addEventListener('click', function () {
     window.location.href = 'https://www.linkedin.com/in/maria-campo-martins-78b302251';
+});
+
+const toggle = document.getElementById('theme-toggle');
+const cv = document.getElementById('cv-icon');
+const github = document.getElementById('github-icon');
+const linkedin = document.getElementById('linkedin-icon');
+
+toggle.addEventListener('change', () => {
+  const isDark = toggle.checked;
+  document.documentElement.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  cv.src = isDark ? './images/cv-light.png' : './images/cv-dark.png';
+  linkedin.src = isDark ? './images/linkedin-light.png' : './images/linkedin-dark.png';
+  github.src = isDark ? './images/github-light.png' : './images/github-dark.png';
+});
+
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('navbar').classList.toggle('show');
 });
